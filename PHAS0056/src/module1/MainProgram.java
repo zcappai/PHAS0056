55be77c550357e6641b8517987661827b9412d03
Akhilesh Pai <zcappai@udxaw-04-01-034.ad.ucl.ac.uk>
2019-10-05 12:30:04 +0100

    Added module1/MainProgram class to display date and time.

---
+++ PHAS0056/src/module1/MainProgram.java
@@ -0,0 +1,14 @@
+package module1;
+import java.util.Date;
+public class MainProgram {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Program starting");
+		Date myDate = new Date();
+		System.out.println(myDate);
+		System.out.println("Program finished");
+
+	}
+
+}
