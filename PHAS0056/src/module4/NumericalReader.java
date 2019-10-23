c96925c07027360f34ff6b8a798c4384db8bd73d
zcappai <zcappai@CT00029162.ad.ucl.ac.uk>
2019-10-23 12:58:03 +0100

    Code completed for module2/NumericalReader. Data file output correctly and numbers correctly printed to screen. No code comments added yet.

--- PHAS0056/src/module4/NumericalReader.java
+++ PHAS0056/src/module4/NumericalReader.java
@@ -4,11 +4,15 @@ import java.net.*;
 import java.util.*;
 
 public class NumericalReader {
-	
+
 	private double minValue;
 	private double maxValue;
-	private double nValues;
+	private int nValues;
 	private double sumOfValues;
+	private File file;
+	private FileWriter f;
+	private BufferedWriter bw;
+	private PrintWriter pw;
 
 	public static String getStringFromKeyboard() throws IOException{
 		InputStreamReader isr = new InputStreamReader(System.in);
@@ -27,22 +31,93 @@ public class NumericalReader {
 	}
 
 	void analysisStart(String dataFile) throws IOException{
-		FileWriter fw = new FileWriter(dataFile);
-		BufferedWriter bw = new BufferedWriter(fw);
-		PrintWriter pw = new PrintWriter(bw);
+		//File outputfile = new File(dataFile);
+		//file = outputfile;
+		f = new FileWriter(dataFile);
+		bw = new BufferedWriter(f);
+		pw = new PrintWriter(bw);
+		
+		minValue = 0;
+		maxValue = 0;
+		nValues = 0;
+		sumOfValues = 0;
 	}
 
-	void analyseData(String line) {
-		
+	void analyseData(String line) throws IOException {
+		Scanner sc = new Scanner(line);
+		if((line.isEmpty() || Character.isLetter(line.charAt(0))) == false) {
+			while(sc.hasNext()) {
+				double token = Double.parseDouble(sc.next());
+				System.out.println(token);
+				//FileWriter fw = new FileWriter(file);
+				//BufferedWriter bw = new BufferedWriter(fw);
+				//PrintWriter pw = new PrintWriter(bw);
+				pw.println(token);
+				sumOfValues += token;
+				nValues++;
+				if(token < minValue) {
+					minValue = token;
+				}
+				else if(token > maxValue) {
+					maxValue = token;
+				}
+			}
+			sc.close();
+		}
+	}
+
+	void analysisEnd() {
+		pw.close();
+		System.out.println("\nMinimum Value is "+minValue);
+		System.out.println("Maximum Value is "+maxValue);
+		System.out.println("Number of Values is "+nValues);
+		System.out.println("Sum of Values is "+sumOfValues);
 	}
 
 	public static void main(String[] args) {
-		NumericalReader x = new NumericalReader();
+
+		NumericalReader nr = new NumericalReader();
+		BufferedReader reader = null;
+		try {
+			reader = nr.brFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_data1.txt");
+		}
+		catch(IOException e1) {
+			System.out.println("Please enter a valid URL!");
+		}
+		String line = "";
+		String saveDir;
+
+		try {
+			saveDir = NumericalReader.getStringFromKeyboard();
+		}
+		catch (IOException e2) {
+			saveDir = System.getProperty("user.home");
+		}
+
+		String saveFile = (saveDir + File.separator + "numbers1.txt");
+
+		try {
+			nr.analysisStart(saveFile); // initialise minValue etc.
+		}
+		catch (IOException e3) {
+			System.out.println(e3);
+		}
+		try {		
+			while ((line = reader.readLine()) != null) {
+				nr.analyseData(line); // analyse lines, check for comments etc.
+			}
+		}
+		catch (IOException e4) {
+			System.out.println(e4);
+		}
+		nr.analysisEnd(); // print min, max, etc.
+
+		/*NumericalReader x = new NumericalReader();
 		try {
 			getStringFromKeyboard();
 		}
 		catch(Exception e1) {
-		}
+		}*/
 
 	}
 
