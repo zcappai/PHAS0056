096869d6cdca045a21e098f878a7fe7911c55349
akpai <akpai@AKHILESH-LAPTOP>
2020-01-29 12:30:49 +0000

    exam2 starting classes. Currently empty.

---
+++ PHAS0056/src/exam2/ExamPart2.java
@@ -0,0 +1,10 @@
+package exam2;
+
+public class ExamPart2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ PHAS0056/src/exam2/ExamPart3.java
@@ -0,0 +1,10 @@
+package exam2;
+
+public class ExamPart3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
